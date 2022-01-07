7dc6eb2b6303405e991a1a9dae8c501f309c8093
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-01-07 19:31:53 +0900

    Add files via upload

---
+++ 예제4-4틀린부분 수정.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 5, b = 5; //변수선언[정수형] + 초기화[대입연산자]
+	int pre, post; //변수선언[정수형]
+
+	pre = (++a) * 3; // 전위형 증감 연산자
+	post = (b++) * 3;// 후위형 증감 연산자
+
+	printf("초깃값 a = %d, b=%d\n", a, b);
+	printf("전위형: (++a)*3 = %d, 후위형: (b++)*3 = %d\n", pre, post);
+
+	return 0;
+}﻿
