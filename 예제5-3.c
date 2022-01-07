4fcea0f16ae5b92276a93df2e2f3eb901a195d9d
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-01-07 19:45:14 +0900

    Add files via upload

---
+++ 예제5-3.c
@@ -0,0 +1,25 @@
+/*5-3. if ~ else if ~ if¹® »ç¿ë*/
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0, b = 0;
+
+	if (a > 0)
+	{
+		b = 1;
+	}
+	else if (a == 0)
+	{
+		b = 2;
+	}
+	else
+	{
+		b = 3;
+	}
+
+	printf("b : %d\n", b);
+
+	return 0;
+}
+
