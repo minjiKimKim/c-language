cc81cfb35ecaa53488e9ca6c3f49837f74a2be35
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-01-10 00:15:45 +0900

    Add files via upload

---
+++ 예제8-6.c
@@ -0,0 +1,14 @@
+/*8-6. 빈칸을 포함한 문자열 입력*/
+#include <stdio.h>
+
+int main(void)
+{
+	char str[80];
+
+	printf("문자열 입력 :");          // 입력 안내 메세지 출력
+	gets(str);                        // 빈칸을 포함한 문자열 입력
+	puts("입력된 문자열 :");          // 문자열 상수 출력
+	puts(str);                        // 배열에 저장된 문자열 출력
+
+	return 0;
+}
